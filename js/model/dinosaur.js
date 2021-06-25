1f218460a1a7fed3c541ad27bbfede4db498ed17
Wonjin Cho <cwonjin11@gmail.com>
2021-06-25 13:21:15 -0400

    select option edit updated

--- js/model/dinosaur.js
+++ js/model/dinosaur.js
@@ -4,6 +4,7 @@ class Dinosaur{    //creating dino for the frontend object  // Javascript Class 
     static all = []     ////  state😏🗃 of dinos  -- On the Frontend    ////  similar to  (@@all)
     // whenever we need to add somthing. To pass as whole object, use curly bracket{} for the attributes
     // The constructor method is a special method of a class for creating and initializing an object of that class.
+    // We Use Our Class Constructor for Any Creation of a Frontend Instance of an Object of this Class 
     // constructor({id, name, image, mezosoic_era_id, diets, height, size, weight, desc}) {
     constructor({id, name, image, mezosoic_era_id, diets, height, size, weight, desc}) {
             this.id = id,
@@ -30,35 +31,60 @@ class Dinosaur{    //creating dino for the frontend object  // Javascript Class 
         cardDiv.classList.add("flip-card")
         cardDiv.setAttribute("data-id", dinosaur.id)
         cardDiv.id = dinosaur.id            //  adding an (id=) to (cardDiv) 
-        cardDiv.innerHTML = this.makeACard()        // use innerHTML to create the inner elements
-        const collectionDiv = document.querySelector("#dino-collection")
-        collectionDiv.append(cardDiv)
+        cardDiv.innerHTML = this.makeACard()  
+        document.querySelector("#dino-collection").append(cardDiv)
+        
+        // use innerHTML to create the inner elements
+        // const collectionDiv = document.querySelector("#dino-collection")
+        // collectionDiv.append(cardDiv)
         cardDiv.addEventListener('click', e => {
             if (e.target.matches(".delete-btn")) this.deleteDino(e)
+
             if (e.target.matches(".edit-btn")){ 
-
                 const dinoToUpdate = e.target.closest(".flip-card")
-                const dinoToEditForm = document.createElement("form")
-                
-                dinoToEditForm.innerHTML = `
+                      console.log("dinoToUpdate.id", dinoToUpdate.id)
+                const dinoToEditForm = document.createElement('form')
+                // const catchEditForm = dinoToEditForm.setAttribute("id", "edit-form")
+                    // console.log("where r you", catchEditForm)
+ 
+                const dinoEraDropDownValue = cardDiv.querySelector(".period").dataset.id
+                // const dinoEraDropDownValue = dinoToEditForm.getElementById('era_dropdown').selectedValue
+                console.log(dinoEraDropDownValue)
 
-                <!--<div class="dinosaur-edit">-->
+                // const dinoEraDropDownValue = dinoToEditForm.querySelector("#era_dropdown")
+                // const dinoEraDropDownValue = cardDiv.querySelector(".period")
+            
+                // console.log(dinoEraDropDownValue)
+                            // console.log("GET THE DROPDOWN VALUE", dinoEraDropDownValue)
+        
+         
+                dinoToEditForm.innerHTML = `
 
                     <h2> 🦕 Edit this dino 🦕 </h2>
                     
-                    <form>
+                    <form class="dino-edit-form">
+
                     <h3>Dino name:</h3>
                     <input type="text" name="name"
                     value="${cardDiv.querySelector("h2").innerText}"
                     placeholder="${cardDiv.querySelector("h2").innerText}"
-                    class="name-edit"/>
-                    <br>
+                    class="name-edit"/> <br>
                    
                     <h3>Image path:</h3>
                     <input type="text" name="image" 
                     value="${cardDiv.querySelector("img").src}"
                     placeholder="${cardDiv.querySelector("img").src}"
                     class="image-edit"/> <br> 
+                    
+                    <h3>Era:</h3>
+                    <select id="era_dropdown" name="period" class="era-edit">
+                    <option value="${this.mezosoic_era_id}" selected> ${this.mezosoic_era_id} </option>
+                    <option value="1">Triassic</option>
+                    <option value="2">Jurassic</option>
+                    <option value="3">Cretaceous</option>
+                    <!--<option value="" disabled selected>Lived...</option>-->
+                    </select>
+                    
 
                     <h3>Height:</h3>
                     <input type="text" name="height"
@@ -92,7 +118,6 @@ class Dinosaur{    //creating dino for the frontend object  // Javascript Class 
                     </form>
               
                     <br><br><br>
-                <!--</div>-->
                 `      
                 // console.log(">>>>>>>", dinoToEditForm) 
 
@@ -110,7 +135,9 @@ class Dinosaur{    //creating dino for the frontend object  // Javascript Class 
                     if(event.target.matches(".submit-button")) {
                         let editedName = dinoToEditForm.querySelector(".name-edit").value
                         let editedImage = dinoToEditForm.querySelector(".image-edit").value
-                        // let editedPeriod = dinoToEditForm.querySelector("select#period-edit").value
+                        let editedEra = dinoToEditForm.querySelector(".era-edit").value
+                            console.log("yoyo", editedEra)
+                        
                         let editedHeight = dinoToEditForm.querySelector(".height-edit").value
                         let editedSize = dinoToEditForm.querySelector(".size-edit").value
                         let editedWeight = dinoToEditForm.querySelector(".weight-edit").value
@@ -119,16 +146,17 @@ class Dinosaur{    //creating dino for the frontend object  // Javascript Class 
                         const dinoObject = {
                             name: editedName,
                             image: editedImage,
+                            mezosoic_era_id: editedEra,
                             height: editedHeight,
                             size: editedSize,
                             weight: editedWeight,
                             desc: editedDescription
                         }
 
-                        // const dinoID = dinoToUpdate.id
-                        const id = e.target.dataset.id
-                        // fetch(`http://localhost:3000/dinosaurs/${dinoID}`, {`
-                        fetch(`${API.ALL_DINOSAURS_URL}/${id}`, {
+                        const dinoID = dinoToUpdate.id
+                        // const id = e.target.dataset.id
+                        fetch(`http://localhost:3000/dinosaurs/${dinoID}`, {
+                        // fetch(`${API.ALL_DINOSAURS_URL}/${id}`, {
                             method: "PATCH",
                             headers: { "Content-Type": "application/json" },
                             body: JSON.stringify(dinoObject)
@@ -138,6 +166,9 @@ class Dinosaur{    //creating dino for the frontend object  // Javascript Class 
                             dinoToUpdate.querySelector("h2").innerText = editedDino.name
                             dinoToUpdate.querySelector("h3").innerText = editedDino.name
                             dinoToUpdate.querySelector("img").src = editedDino.image
+                            dinoToUpdate.querySelector(".period").dataset.id = editedDino.mezosoic_era_id
+                            console.log(editedDino.mezosoic_era_id)
+                            console.log(dinoToUpdate.querySelector(".period").dataset.id)
                             dinoToUpdate.querySelector(".height").innerText = editedDino.height
                             dinoToUpdate.querySelector(".size").innerText = editedDino.size
                             dinoToUpdate.querySelector(".weight").innerText = editedDino.weight
@@ -145,24 +176,11 @@ class Dinosaur{    //creating dino for the frontend object  // Javascript Class 
                         })
                         .then(dinoToEditForm.remove())
                         cardDiv.querySelector(".edit-btn").disabled = false
-
                     }
-
-
                 })
-
-
-
             } //this.editDino(e)
-                
-             
         })
     }
-
-
-
-
-
 
 
     makeACard = () =>{ //console.log(this)
@@ -170,11 +188,11 @@ class Dinosaur{    //creating dino for the frontend object  // Javascript Class 
         let eraName
             if (this.mezosoic_era_id == 1) {eraName = "triassic"}
             else if (this.mezosoic_era_id == 2) {eraName = "Jurassic"}
-            else {eraName = "Crateteous"}
+            else {eraName = "Cretaceous"}
 
         return `
-       
-            <div class="flip-card">
+        <!-- <div class="flip-card"> -->
+          
                 <div class="flip-card-inner">
                     <div class="flip-card-front" >
                         <img src=${this.image} class="dino-icon" />
@@ -185,7 +203,7 @@ class Dinosaur{    //creating dino for the frontend object  // Javascript Class 
                         <h3 style="color: #fdc52c;">${this.name}</h3>
                         Diet : <p style="display:inline" class="diet">${this.diets}</p><br>
                         <!-- <p>period : ${this.mezosoic_era_id}</p> -->
-                        Period : <p style="display:inline" class="period">${this.mezosoic_era_id = eraName}</p><br>
+                        Period : <p data-id="${this.mezosoic_era_id}" style="display:inline" class="period">${this.mezosoic_era_id=eraName}</p><br>
                         Height : <p style="display:inline" class="height">${this.height}</p><br>
                         Length : <p style="display:inline" class="size">${this.size}</p><br>
                         Weight : <p style="display:inline" class="weight">${this.weight}</p><br>
@@ -195,13 +213,14 @@ class Dinosaur{    //creating dino for the frontend object  // Javascript Class 
                   
                     </div>
                 </div>
-            </div>
+                
+              <!--  </div>--> 
         
                 
         `
     }  //makeACard end
    
-// Delete Dinosaur ####
+// Delete a Dinosaur ####
     deleteDino(e) {
         const id = e.target.dataset.id
         const byeDino = document.getElementById(id)
